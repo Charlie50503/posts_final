d63565c0e5a0a8c0fcbf944576f7aae6a9736f67
charlie50503 <cate50503@gmail.com>
2022-08-28 23:55:54 +0900

    feat:添加line callback 代碼

--- routes/auth.js
+++ routes/auth.js
@@ -47,4 +47,27 @@ router.get(
     authController.line.auth(req, res, next);
   }
 );
+router.get(
+  "/line/callback",
+  (req, res, next) => {
+    authController.line.execCallback(req, res, next);
+  },
+  /**
+    * #swagger.ignore = true
+    */
+  handleErrorAsync(async (req, res, next) => {
+    if (req.user) {
+      const token = await generateJwtToken(req.user.id);
+
+      if (token) {
+        res.cookie("line-token", token);
+        res.redirect(`${process.env.FRONTEND_REDIRECT_URL}?token=${token}&from=google`);
+      } else {
+        return next(appError("400", "40003", "無生成 Token 的權限"));
+      }
+
+    } else {
+      return next(appError("401", "40001", "Google 認證錯誤"));
+    }
+  }));
 module.exports = router;
